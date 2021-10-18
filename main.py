2ecc225697c524472c91f1a6f0dc945bb914ce94
Safak Inan <safak.inan61@hotmail.com>
2021-10-18 17:07:24 +0200

    initial cringe

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from components import base
+
+
+st.set_page_config(
+        page_title='Youboard',
+        layout='wide',
+        initial_sidebar_state="expanded"
+    )
+
+if __name__ == "__main__":
+    base.main()
